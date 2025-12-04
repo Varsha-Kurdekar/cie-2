b6953470f52bc605975a46ae1045f51310b1adad
Varsha-Kurdekar <varshavkurdekar@gmail.com>
2025-12-04 16:33:50 +0530

    save

---
+++ atm.py
@@ -0,0 +1,26 @@
+import sys
+
+if len(sys.argv) == 4:
+    withdrawal = sys.argv[1]
+    balance = sys.argv[2]
+    pin = sys.argv[3]
+    print("User provided inputs.")
+    
+else:
+    withdrawal = 1000      
+    balance = 500       
+    pin = 12345 
+    print("No inputs given - using default values.")  
+    
+    print("ATM details\n")  
+    print("Withdrawal:",withdrawal) 
+    print("Balance:",balance)   
+    print("PIN:",pin)       
+
+if pin == 12345:
+    if withdrawal <= balance:
+        print("Transaction Approved")
+    else:
+        print("Transaction Declined: Insufficient Balance")
+else:
+    print("Transaction Declined: Incorrect PIN")
